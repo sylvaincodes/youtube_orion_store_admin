4ef6b33e4a00f7fb1a7fcd24897dde8b0e16a22e
sylvaincodes <adandesylvain2010@gmail.com>
2024-12-10 19:54:01 +0100

    Update workflow - Add Jest Test component Add Cypress Test component

---
+++ cypress/component/HomePage.cy.tsx
@@ -0,0 +1,8 @@
+//This project use Nextjs 15 with React 19.
+// Downgrade to React 18 if you want to lauch these tests
+
+describe("HomePage.cy.tsx", () => {
+  it("playground", () => {
+    // cy.mount()
+  });
+});
